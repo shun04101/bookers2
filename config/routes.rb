1a07df55359e03ddd81519d442c7124442acb4d6
EC2 Default User <ec2-user@ip-172-31-27-1.ec2.internal>
2021-11-10 12:54:34 +0000

    [Add]4-10 Bootstrap導入

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books, only:[:create, :index, :show, :edit, :update, :destroy]
   resources :users, only:[:new, :create, :show, :edit, :update]
+  resources :homes
 end
