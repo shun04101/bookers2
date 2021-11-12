b1142b04703f004cab516874025b270155e18198
EC2 Default User <ec2-user@ip-172-31-27-1.ec2.internal>
2021-11-12 08:11:53 +0000

    [Update]4-10 Bootstrap適用04

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -19,6 +20,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render 'edit'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'home/index' => 'homes#index'
+  get 'home/about' => 'homes#about'
   resources :books, only:[:create, :index, :show, :edit, :update, :destroy]
   resources :users, only:[:index, :show, :edit, :update]
 end
